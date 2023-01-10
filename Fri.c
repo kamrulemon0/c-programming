9b1bc99950a238d1d93457b4f13c784c9d7897ee
kamrulemon0 <kamrulemon009911@gmail.com>
2023-01-10 11:55:43 +0600

    \n

--- Fri.c
+++ Fri.c
@@ -1,7 +1,15 @@
 #include<stdio.h>
 
 //This code display how types work in c
+
+// \n is new line 
+
 int main(){
     
-    printf("Hello");
+    printf("BUBt\n");
+    printf("Bangladesh\n");
+    printf("BUBT\n");
+    printf("BUBT\n");
+
+    return 0;
 }
