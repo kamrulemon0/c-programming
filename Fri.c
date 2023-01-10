0486f7da56dcf64f989362227da2dcf4ae5c4f60
kamrulemon0 <kamrulemon009911@gmail.com>
2023-01-10 16:36:43 +0600

    QS-2

--- Fri.c
+++ Fri.c
@@ -7,11 +7,23 @@
 // char = %c
 // float = %f
 // scanf ("%d",& age);
-int main(){  // Qs-1: write a program to calculate are of a square (side is given)
+int hain(){  // Qs-1: write a program to calculate are of a square (side is given)
 
     int side;
     printf("Enter side");
     scanf("%d",&side);
     printf("area is : %d", side*side);
     return 0;
+};
+
+// Q2 : write a program to calculate area of a circle
+
+int main(){
+float radius;
+printf("Enter radius");
+scanf("%f",&radius);
+
+printf("area is : %f", 3.1416*radius*radius);
+
+return 0;
 }
