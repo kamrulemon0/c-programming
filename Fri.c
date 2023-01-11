86f4a6342ca130cb120696598240412f04779a08
kamrulemon0 <kamrulemon009911@gmail.com>
2023-01-11 21:59:36 +0600

    Power

--- Fri.c
+++ Fri.c
@@ -18,7 +18,7 @@ int hain(){  // Qs-1: write a program to calculate are of a square (side is give
 
 // Q2 : write a program to calculate area of a circle
 
-int main(){
+int kain(){
 float radius;
 printf("Enter radius");
 scanf("%f",&radius);
@@ -26,4 +26,20 @@ scanf("%f",&radius);
 printf("area is : %f", 3.1416*radius*radius);
 
 return 0;
+}
+// instructions 
+// These are statements in  program (Type -1.Type Declaration ,2.Arithmetic instruction .3.control instructions)
+//Arithmetic instructions [Note -single variable on the LHS]
+
+
+#include<math.h>
+int main(){
+
+int b, c;
+b=c=1;
+int a =b +c;
+int power = b^c;
+printf("%d",power);
+return 0;
+
 }
