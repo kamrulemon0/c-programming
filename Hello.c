99f557c1b0f903ab4ed91ab21285ca235b1b6917
kamrulemon0 <kamrulemon009911@gmail.com>
2023-01-14 20:34:01 +0600

    Conditional -1

--- Hello.c
+++ Hello.c
@@ -1,4 +1,18 @@
 // Conditional statement
 
 #include<stdio.h>
+int main(){
+    int gf;
+    printf("How Many girlfriend have you");
+    scanf("%d",&gf);
 
+    if(gf<4){
+        printf("You are a good Boy\n");
+
+    }
+    else{
+        ("Bad boy\n");
+    }
+
+    return 0;
+}
