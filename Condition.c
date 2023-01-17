e4f709871082261f939926148ba9149bb39a4764
kamrulemon0 <kamrulemon009911@gmail.com>
2023-01-17 21:31:28 +0600

    Printf

--- Condition.c
+++ Condition.c
@@ -71,20 +71,38 @@ if(number >= 0) {
 }
 
 
+// int lain(){
+    
+//     int marks;
+//     printf("enter number(0-100):");
+//     scanf("%d", &marks);
+
+// if(marks<=30){
+//     printf("Fail\n");
+  
+// }else if(marks >30 && marks <=){
+//     printf("pass");
+
+// }
+// else{
+//     printf("Try again");
+// }
+// return 0;
+// }
+
+
 int main(){
     int marks;
-    printf("enter number(0-100):");
+    printf("enter number");
     scanf("%d", &marks);
 
-if(marks<=30){
-    printf("Fail\n");
-  
-}else if(marks >30 && marks <=100){
-    printf("pass");
-
-}
-else{
-    printf("Try again");
-}
-return 0;
+    if(marks < 30){
+        printf("c\n");
+    }else if(marks >= 30 && marks < 70){
+        printf("B\n");
+    }else if(marks >=70 && marks <90){
+        printf("A\n");
+    }else{
+        printf("A+");
+    }
 }
