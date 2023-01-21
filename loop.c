ffb65b98d3d2be0db3d50a06999b5cdadbb988cc
kamrulemon0 <kamrulemon009911@gmail.com>
2023-01-21 23:52:11 +0600

    QS

--- loop.c
+++ loop.c
@@ -1,4 +1,17 @@
 #include<stdio.h>
+int kain(){
+    //iterator ; counter
+    for(int i=1; i<=5; i=i+1 ){
+        printf("Hello Thor\n");
+    }
+    
+                       // Loop control instructions [ type -1-for,2-while,3-do while]
+}                     //for(initialisation,condition; updation)
+
+
+// Qs. print the numbers from 0 to 10
 int main(){
-    printf("Hello"); // Loop control instructions [ type -1-for,2-while,3-do while]
+    for(int i =0; i<=10; i=i+1){
+        printf("%d\n", i);
+    }
 }
