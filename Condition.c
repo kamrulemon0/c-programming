c82675200463e5b672a0599cc9cb02c1fbc1f7bf
kamrulemon0 <kamrulemon009911@gmail.com>
2023-01-21 23:34:43 +0600

    Loop

--- Condition.c
+++ Condition.c
@@ -1,74 +1,74 @@
-#include<stdio.h>
-int aain(){
-    int age;
-    printf("enter age :");
-    scanf("%d",&age);
+// #include<stdio.h>
+// int aain(){
+//     int age;
+//     printf("enter age :");
+//     scanf("%d",&age);
 
-    if(age>=18){
+//     if(age>=18){
 
-        printf("adult \n");
+//         printf("adult \n");
 
-    }
-    else if(age > 13 && age <18){
-        printf("Hello world");
-    }
-    else {
-        printf("child");
-    }
-}
+//     }
+//     else if(age > 13 && age <18){
+//         printf("Hello world");
+//     }
+//     else {
+//         printf("child");
+//     }
+// }
 
-// switch
+// // switch
 
-int kain(){
- int day;
- printf("enter day(1-7) :");
- scanf("%d",&day);
+// int kain(){
+//  int day;
+//  printf("enter day(1-7) :");
+//  scanf("%d",&day);
 
-switch(day){
+// switch(day){
 
-    case 1 : printf("Emonday\n");
-         break;
-    case 2 : printf("Hello day\n");
-          break;
-    case 3 : printf("monday\n");
-         break;
-    case 4 : printf("oday\n");
-         break;
-    case 5 : printf("day\n");
-         break;
+//     case 1 : printf("Emonday\n");
+//          break;
+//     case 2 : printf("Hello day\n");
+//           break;
+//     case 3 : printf("monday\n");
+//          break;
+//     case 4 : printf("oday\n");
+//          break;
+//     case 5 : printf("day\n");
+//          break;
 
-  default : printf("Not a valid day \n");
+//   default : printf("Not a valid day \n");
 
-}
+// }
 
 
 
- return 0;
+//  return 0;
 
-}
+// }
 
-int pain(){
-int number;
-printf("enter number :");
-scanf("%d",number);
+// int pain(){
+// int number;
+// printf("enter number :");
+// scanf("%d",number);
 
-if(number >= 0) {
-    printf("Positive \n");
+// if(number >= 0) {
+//     printf("Positive \n");
 
-    if(number % 2==0){
-        printf("even\n");
+//     if(number % 2==0){
+//         printf("even\n");
 
-    }else{
-        printf("odd\n");
-    }
+//     }else{
+//         printf("odd\n");
+//     }
 
-} else {
-    printf("Negative \n");
-}
+// } else {
+//     printf("Negative \n");
+// }
 
-  return 0;
+//   return 0;
 
-}
+// }
 
 
 // int lain(){
@@ -91,18 +91,39 @@ if(number >= 0) {
 // }
 
 
-int main(){
-    int marks;
-    printf("enter number");
-    scanf("%d", &marks);
+// int pain(){
+//     int marks;
+//     printf("enter number");
+//     scanf("%d", &marks);
 
-    if(marks < 30){
-        printf("c\n");
-    }else if(marks >= 30 && marks < 70){
-        printf("B\n");
-    }else if(marks >=70 && marks <90){
-        printf("A\n");
-    }else{
-        printf("A+");
-    }
-}
+//     if(marks < 30){
+//         printf("c\n");
+//     }else if(marks >= 30 && marks < 70){
+//         printf("B\n");
+//     }else if(marks >=70 && marks <90){
+//         printf("A\n");
+//     }else{
+//         printf("A+");
+//     }
+// }
+
+// int main(){
+//     char chr;
+//     printf("enter your value\n");
+//     scanf("%c",&chr);
+
+//     if(chr >= "A" && chr <= "Z"){
+//         printf("Upper-case\n");
+//     }
+//     else if(chr >="a" && chr <= "z"){
+//         printf("lower case\n");
+//     }
+//     else{
+//         printf("Hello, Try  lot\n");
+//     }
+
+//     return 0;
+// }
+
+// Loop contorl
+
---
+++ loop.c
@@ -0,0 +1,4 @@
+#include<stdio.h>
+int main(){
+    printf("Hello"); // Loop control instructions [ type -1-for,2-while,3-do while]
+}
